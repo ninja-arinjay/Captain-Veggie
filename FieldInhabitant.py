e95bbf87d1088e2d78f4f9aff4366155553b5890
Arinjay Panwar <arinjaypanwararp@gmail.com>
2023-12-04 21:51:57 -0500

    Update FieldInhabitant.py

--- FieldInhabitant.py
+++ FieldInhabitant.py
@@ -1,1 +1,9 @@
-#FieldInhabitant.py
+class FieldInhabitant:
+    def __init__(self, symbol):
+        self._symbol = symbol
+
+    def get_symbol(self):
+        return self._symbol
+
+    def set_symbol(self, symbol):
+        self._symbol = symbol

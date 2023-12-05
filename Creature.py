fe498bc43ada99779185164b45ea11028d9bb966
Arinjay Panwar <arinjaypanwararp@gmail.com>
2023-12-04 21:53:41 -0500

    Update Creature.py

--- Creature.py
+++ Creature.py
@@ -1,1 +1,20 @@
-#Creature.py
+from FieldInhabitant import FieldInhabitant
+
+
+class Creature(FieldInhabitant):
+    def __init__(self, x, y, symbol):
+        super().__init__(symbol)
+        self._x = x
+        self._y = y
+
+    def get_x(self):
+        return self._x
+
+    def set_x(self, x):
+        self._x = x
+
+    def get_y(self):
+        return self._y
+
+    def set_y(self, y):
+        self._y = y

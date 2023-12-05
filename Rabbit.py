df3121f2f6c7e1284c3d65ea8e602a50e8c3d501
Arinjay Panwar <arinjaypanwararp@gmail.com>
2023-12-04 21:55:01 -0500

    Update Rabbit.py

--- Rabbit.py
+++ Rabbit.py
@@ -1,1 +1,18 @@
-#Rabbit.py
+from Creature import Creature
+
+
+class Rabbit(Creature):
+    def __init__(self, x, y):
+        super().__init__(x, y, "R")
+
+    def get_x(self):
+        return super().get_x()
+
+    def set_x(self, x):
+        super().set_x(x)
+
+    def get_y(self):
+        return super().get_y()
+
+    def set_y(self, x):
+        super().set_y(x)

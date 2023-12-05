71af755dd150c680dc73f2920f5f9f4d1f863b15
Arinjay Panwar <arinjaypanwararp@gmail.com>
2023-12-04 21:54:26 -0500

    Update Captain.py

--- Captain.py
+++ Captain.py
@@ -1,1 +1,25 @@
-#Captain.py
+from Creature import Creature
+
+
+class Captain(Creature):
+    def __init__(self, x, y):
+        super().__init__(x, y, "V")
+        self._veggies_collected = []
+
+    def get_x(self):
+        return super().get_x()
+
+    def set_x(self, x):
+        super().set_x(x)
+
+    def get_y(self):
+        return super().get_y()
+
+    def set_y(self, x):
+        super().set_y(x)
+
+    def addVeggie(self, veggie):
+        self._veggies_collected.append(veggie)
+
+    def get_veggies_collected(self):
+        return self._veggies_collected
